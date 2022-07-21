dc576d6cebfa50acda4a64ac9d56511a06e99986
Erfan Abdi <erfangplus@gmail.com>
2022-07-21 18:02:27 +0430

    base: Introduce LiveDisplay from Lineage
    Change-Id: Ie3a61a67edfc4f4568fa2341791fa9fa5263f90d
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -290,6 +290,7 @@ import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
 import com.android.server.bliss.LineageGlobalActionsService;
 import com.android.server.bliss.health.HealthInterfaceService;
+import com.android.server.bliss.display.LiveDisplayService;
 
 import dalvik.system.VMRuntime;
 
@@ -2678,6 +2679,10 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartHealthService");
             mSystemServiceManager.startService(HealthInterfaceService.class);
+            t.traceEnd();
+
+            t.traceBegin("StartLiveDisplayService");
+            mSystemServiceManager.startService(LiveDisplayService.class);
             t.traceEnd();
         }
 
