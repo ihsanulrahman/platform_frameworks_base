09acd5d1bfa1a05c6328e8252185c62301834293
Dhina17 <dhinalogu@gmail.com>
2024-03-21 14:57:35 +0000

    fixup! base: Add support for parallel space
    Change-Id: I512609a24204303b49a3aa0bb1db387ca3d1ff61
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- services/core/java/com/android/server/trust/TrustManagerService.java
+++ services/core/java/com/android/server/trust/TrustManagerService.java
@@ -84,6 +84,7 @@ import com.android.internal.content.PackageMonitor;
 import com.android.internal.infra.AndroidFuture;
 import com.android.internal.util.DumpUtils;
 import com.android.internal.widget.LockPatternUtils;
+import com.android.server.LocalServices;
 import com.android.server.SystemService;
 import com.android.server.servicewatcher.CurrentUserServiceSupplier;
 import com.android.server.servicewatcher.ServiceWatcher;
