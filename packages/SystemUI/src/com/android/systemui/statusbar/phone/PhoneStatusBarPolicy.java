247ad98c16ef858bc49574b1c66519717e786cee
Pranav Vashi <neobuddy89@gmail.com>
2024-08-27 22:26:15 +0530

    fixup! SystemUI: Refactor statusbar bluetooth icon
    * This is handled by StatusBarBluetoothView itself.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -180,7 +180,6 @@ public class PhoneStatusBarPolicy
     private final Context mContext;
 
     private boolean mShowBluetoothBattery;
-    private boolean mHideBluetooth;
 
     @Inject
     public PhoneStatusBarPolicy(Context context, StatusBarIconController iconController,
@@ -264,8 +263,7 @@ public class PhoneStatusBarPolicy
         mDateFormatUtil = dateFormatUtil;
 
         Dependency.get(TunerService.class).addTunable(this,
-                BLUETOOTH_SHOW_BATTERY,
-                StatusBarIconController.ICON_HIDE_LIST);
+                BLUETOOTH_SHOW_BATTERY);
     }
 
     /** Initialize the object after construction. */
@@ -413,14 +411,6 @@ public class PhoneStatusBarPolicy
                         TunerService.parseIntegerSwitch(newValue, true);
                 updateBluetooth();
                 break;
-            case StatusBarIconController.ICON_HIDE_LIST:
-                ArraySet<String> hideList = StatusBarIconController.getIconHideList(mContext, newValue);
-                boolean hideBluetooth = hideList.contains(mSlotBluetooth);
-                if (hideBluetooth != mHideBluetooth) {
-                    mHideBluetooth = hideBluetooth;
-                    updateBluetooth();
-                }
-                break;
             default:
                 break;
         }
@@ -531,7 +521,7 @@ public class PhoneStatusBarPolicy
         }
 
         mIconController.setBluetoothIcon(mSlotBluetooth,
-                new BluetoothIconState(!mHideBluetooth && bluetoothVisible, batteryLevel, contentDescription));
+                new BluetoothIconState(bluetoothVisible, batteryLevel, contentDescription));
     }
 
     private final void updateTTY() {
