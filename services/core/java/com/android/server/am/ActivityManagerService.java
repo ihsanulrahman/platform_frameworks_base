87a9ef21c531609d0373321e694fbb9e66170fd5
LibXZR <i@xzr.moe>
2024-11-20 12:50:54 +0800

    base: Add support for strict standby policy [SQUASH]
    When dismissing from recents, this will force stop apps with restricted
    battery usage and put them into RESTRICTED standby bucket. Also all
    restrictions will be imposed even when device is charging.
    
    This can be useful to kill dirty vigorous Chinese apps.
    
    [ghostrider-reborn]:
    - Add more restrictions (jobs, alarms)
    - Show toast only in ENG build
    - Remove settings toggle
    
    Change-Id: I8f075ef48c5775971e3e0b8b1ea843d70a2e44c4
    
     BroadcastQueue: Skip delivering broadcasts to restricted apps
    
    Fixes plenty of timeouts blocking the whole queue.
    
    Also strict standby is enhanced that nothing could wakeup a
    static broadcast receiver.
    
    Change-Id: I7b748a63287319e765d1548b28987f56be5e599e
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    [forward ported for Android 13,14,15]
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I17a79670d924d348de5898fd02ebf5e43deaf607
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -4904,7 +4904,7 @@ public class ActivityManagerService extends IActivityManager.Stub
             if (!mConstants.mEnableWaitForFinishAttachApplication) {
                 finishAttachApplicationInner(startSeq, callingUid, pid);
             }
-            maybeSendBootCompletedLocked(app, isRestrictedBackupMode);
+            maybeSendBootCompletedLocked(app, isRestrictedBackupMode, shouldSkipBootCompletedBroadcastForPackage(app.getApplicationInfo()));
         } catch (Exception e) {
             // We need kill the process group here. (b/148588589)
             Slog.wtf(TAG, "Exception thrown during bind of " + app, e);
@@ -5149,7 +5149,8 @@ public class ActivityManagerService extends IActivityManager.Stub
      * Send LOCKED_BOOT_COMPLETED and BOOT_COMPLETED to the package explicitly when unstopped,
      * or when the package first starts in private space
      */
-    private void maybeSendBootCompletedLocked(ProcessRecord app, boolean isRestrictedBackupMode) {
+    private void maybeSendBootCompletedLocked(ProcessRecord app, 
+            boolean isRestrictedBackupMode, boolean shouldSkipBootCompletedBroadcastForPackage) {
         boolean sendBroadcast = false;
         if (android.os.Flags.allowPrivateProfile()
                 && android.multiuser.Flags.enablePrivateSpaceFeatures()) {
@@ -5176,7 +5177,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
 
         // Don't send BOOT_COMPLETED if currently in restricted backup mode
-        if (isRestrictedBackupMode) return;
+        if (isRestrictedBackupMode || shouldSkipBootCompletedBroadcastForPackage) return;
 
         if (!sendBroadcast) {
             if (!android.content.pm.Flags.stayStopped()) return;
@@ -21258,5 +21259,11 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     public boolean shouldForceCutoutFullscreen(String packageName) {
         return mActivityTaskManager.shouldForceCutoutFullscreen(packageName);
+    }
+    
+    public boolean shouldSkipBootCompletedBroadcastForPackage(ApplicationInfo info) {
+        return getAppOpsManager().checkOpNoThrow(
+                AppOpsManager.OP_RUN_ANY_IN_BACKGROUND,
+                info.uid, info.packageName) != AppOpsManager.MODE_ALLOWED;
     }
 }
