241ce5481e623171505db015bd937c0a98a99961
SuperDroidBond <superdroidbond@yahoo.com>
2024-11-24 11:42:17 +0000

    SystemUI: UdfpsController: Disable Night Light on keyguard
    Originally "UDFPS: Night Light Disable"
    
    SuperDroidBond: the setting option became useless in 13 so better keep only overlay and enable it by default.
    
    FOD: allow user to disable night light when active [1/2]
    Make this optional for some devices and config for default true on devices which weird light sensors on FOD
    eg: OP7TPRO
    
    Remove FOD night light if not enabled by default [1/2]
    
    SDB: adapt it for A12 UDFPS by Google
    
    @Stallix: Adapt for 12L and use UdfpsUtils for config checking instead
    
    Co-authored-by: Varun Date <date.varun123@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Change-Id: Ib54251b60aafc62877f41ac55f42411f8eb21064
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -41,6 +41,7 @@ import android.graphics.Rect;
 import android.hardware.biometrics.BiometricFingerprintConstants;
 import android.hardware.biometrics.BiometricPrompt;
 import android.hardware.biometrics.SensorProperties;
+import android.hardware.display.ColorDisplayManager;
 import android.hardware.display.DisplayManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.hardware.fingerprint.FingerprintSensorProperties;
@@ -76,6 +77,7 @@ import com.android.internal.R;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.InstanceId;
 import com.android.internal.util.LatencyTracker;
+import com.android.internal.util.bliss.udfps.UdfpsUtils;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.systemui.Dumpable;
 import com.android.systemui.animation.ActivityTransitionAnimator;
@@ -244,6 +246,10 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private boolean mSmartPixelsEnabled;
     private boolean mSmartPixelsOnPowerSave;
 
+    private boolean mDisableNightMode;
+    private boolean mNightModeActive;
+    private int mAutoModeState;
+
     @VisibleForTesting
     public static final VibrationAttributes UDFPS_VIBRATION_ATTRIBUTES =
             new VibrationAttributes.Builder()
@@ -312,6 +318,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         @Override
         public void showUdfpsOverlay(long requestId, int sensorId, int reason,
                 @NonNull IUdfpsOverlayControllerCallback callback) {
+            if (mDisableNightMode) {
+                disableNightMode();
+            }
             mUdfpsOverlayInteractor.setRequestId(requestId);
             mFgExecutor.execute(() -> UdfpsController.this.showUdfpsOverlay(
                     new UdfpsControllerOverlay(
@@ -354,6 +363,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
         @Override
         public void hideUdfpsOverlay(int sensorId) {
+            if (mDisableNightMode) {
+                setNightMode(mNightModeActive, mAutoModeState);
+            }
             mFgExecutor.execute(() -> {
                 if (mKeyguardUpdateMonitor.isFingerprintDetectionRunning()) {
                     // if we get here, we expect keyguardUpdateMonitor's fingerprintRunningState
@@ -867,6 +879,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         udfpsShell.setUdfpsOverlayController(mUdfpsOverlayController);
         mUdfpsVendorCode = mContext.getResources().getInteger(com.android.systemui.res.R.integer.config_udfpsVendorCode);
         mDisableSmartPixels = mContext.getResources().getBoolean(com.android.systemui.res.R.bool.config_disableSmartPixelsOnUDFPS);
+        mDisableNightMode = UdfpsUtils.hasUdfpsSupport(mContext);
         boolean screenOffFodSupported = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_supportScreenOffUdfps) ||
                 !TextUtils.isEmpty(mContext.getResources().getString(
@@ -944,6 +957,23 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             Settings.System.putIntForUser(mContext.getContentResolver(),
                     Settings.System.SMART_PIXELS_ON_POWER_SAVE,
                     1, UserHandle.USER_CURRENT);
+        }
+    }
+
+    private void disableNightMode() {
+        ColorDisplayManager colorDisplayManager = mContext.getSystemService(ColorDisplayManager.class);
+        mAutoModeState = colorDisplayManager.getNightDisplayAutoMode();
+        mNightModeActive = colorDisplayManager.isNightDisplayActivated();
+        colorDisplayManager.setNightDisplayActivated(false);
+    }
+
+    private void setNightMode(boolean activated, int autoMode) {
+        ColorDisplayManager colorDisplayManager = mContext.getSystemService(ColorDisplayManager.class);
+        colorDisplayManager.setNightDisplayAutoMode(0);
+        if (autoMode == 0) {
+            colorDisplayManager.setNightDisplayActivated(activated);
+        } else if (autoMode == 1 || autoMode == 2) {
+            colorDisplayManager.setNightDisplayAutoMode(autoMode);
         }
     }
 
